582b5a4cb2421231d95c5382a646cb3a07917d83
sundzibo <d.z.sun@lancaster.ac.uk>
2024-01-10 01:44:48 +0100

    Green level. Need to finish the modifier apples on this level.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -25,19 +25,19 @@ public class LevelManager : MonoBehaviour
     private void Start()
     {
         RectTransform rt = (RectTransform)displayIcon.transform;
-
+        // display all icons
         for (int i = 1; i <= totalScore; i++) {
-            float rowOffset = rt.rect.width * i;
-            Vector3 position = new Vector3(transform.position.x - rowOffset, transform.position.y, transform.position.z);
+            float rowOffset = rt.rect.width * i;    // the offset of the current icon to the starting point
+            Vector3 position = new Vector3(transform.position.x - rowOffset, transform.position.y, transform.position.z); // the position of the current icon
             Image img = Instantiate(displayIcon, position, Quaternion.identity);
-            img.transform.parent = canvas.transform;
+            img.transform.SetParent(canvas.transform);
             scoreList.Add(img.gameObject);
         }
     }
-
+    // remove the last icon
     public void UpdateScore() {
         Destroy(scoreList.Last());
-        if (scoreList.Any())
+        if (scoreList.Any())    // update the list
         {
             scoreList.RemoveAt(scoreList.Count - 1);
         }
